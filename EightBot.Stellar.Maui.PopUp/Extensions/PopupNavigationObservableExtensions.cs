e1aaa5b90817d9fd123154b747fff15ac4cefb8a
Michael Stonis <michael.stonis@eightbot.com>
2022-12-23 20:17:31 -0600

    started porting effects
    effects

---
+++ EightBot.Stellar.Maui.PopUp/Extensions/PopupNavigationObservableExtensions.cs
@@ -0,0 +1,324 @@
+﻿using System.Reactive.Threading.Tasks;
+
+namespace EightBot.Stellar.Maui;
+
+public static class NavigationObservableExtensions
+{
+    private static uint _navigatingCount;
+
+    /* This is roughly 12 UI ticks at 60fps. Slightly more than 200ms */
+    public static TimeSpan DefaultMultiTapThrottleDuration { get; set; }
+        = TimeSpan.FromMilliseconds(17 * 12);
+
+    public static bool Navigating
+    {
+        get => Interlocked.Exchange(ref _navigatingCount, 0) > 0;
+
+        set
+        {
+            if (value)
+            {
+                Interlocked.Increment(ref _navigatingCount);
+                return;
+            }
+
+            Interlocked.Decrement(ref _navigatingCount);
+        }
+    }
+
+    public static IDisposable NavigateToPage<TPage>(
+        this IObservable<Unit> observable,
+        VisualElement element,
+        Action<TPage, Unit> preNavigation = null,
+        Action<TPage, Unit> postNavigation = null,
+        bool animated = true,
+        bool allowMultiple = false,
+        IScheduler pageCreationScheduler = null,
+        TimeSpan? multiTapThrottleDuration = null)
+        where TPage : Page, IStellarView
+    {
+        return NavigateToPage<Unit, TPage>(observable, element, preNavigation, postNavigation, animated, allowMultiple, pageCreationScheduler, multiTapThrottleDuration);
+    }
+
+    public static IDisposable NavigateToPage<TParameter, TPage>(
+        this IObservable<TParameter> observable,
+        VisualElement element,
+        Action<TPage, TParameter> preNavigation = null,
+        Action<TPage, TParameter> postNavigation = null,
+        bool animated = true,
+        bool allowMultiple = false,
+        IScheduler pageCreationScheduler = null,
+        TimeSpan? multiTapThrottleDuration = null)
+        where TPage : Page, IStellarView
+    {
+        return observable
+            .ThrottleFirst(multiTapThrottleDuration ?? DefaultMultiTapThrottleDuration, RxApp.TaskpoolScheduler)
+            .Where(_ => allowMultiple || !Navigating)
+            .Do(_ => Navigating = true)
+            .ObserveOn(pageCreationScheduler ?? RxApp.TaskpoolScheduler)
+            .Select(
+                x =>
+                {
+                    var page = element.GetPage<TPage>();
+                    return (Page: page, Parameter: x, IsAppearingTask: page.AppearingAsync());
+                })
+            .ObserveOn(RxApp.MainThreadScheduler)
+            .SelectMany(
+                async x =>
+                {
+                    preNavigation?.Invoke(x.Page, x.Parameter);
+
+                    await Task.WhenAll(
+                        x.IsAppearingTask,
+                        element.Navigation.PushAsync(x.Page, animated));
+
+                    postNavigation?.Invoke(x.Page, x.Parameter);
+
+                    return Unit.Default;
+                })
+            .Do(_ => Navigating = false)
+            .Subscribe();
+    }
+
+    public static IDisposable NavigateToPage<TParameter, TPage>(
+        this IObservable<TParameter> observable,
+        VisualElement element,
+        Func<TParameter, TPage> pageCreator,
+        Action<TPage, TParameter> preNavigation = null,
+        Action<TPage, TParameter> postNavigation = null,
+        bool animated = true,
+        bool allowMultiple = false,
+        IScheduler pageCreationScheduler = null,
+        TimeSpan? multiTapThrottleDuration = null)
+        where TPage : Page, IStellarView
+    {
+        return observable
+            .ThrottleFirst(multiTapThrottleDuration ?? DefaultMultiTapThrottleDuration, RxApp.TaskpoolScheduler)
+            .Where(_ => allowMultiple || !Navigating)
+            .Do(_ => Navigating = true)
+            .ObserveOn(pageCreationScheduler ?? RxApp.TaskpoolScheduler)
+            .Select(
+                x =>
+                {
+                    var page = pageCreator.Invoke(x);
+                    return (Page: page, Parameter: x, IsAppearingTask: page.AppearingAsync());
+                })
+            .ObserveOn(RxApp.MainThreadScheduler)
+            .SelectMany(
+                async x =>
+                {
+                    preNavigation?.Invoke(x.Page, x.Parameter);
+
+                    await Task.WhenAll(
+                        x.IsAppearingTask,
+                        element.Navigation.PushAsync(x.Page, animated));
+
+                    postNavigation?.Invoke(x.Page, x.Parameter);
+
+                    return Unit.Default;
+                })
+            .Do(_ => Navigating = false)
+            .Subscribe();
+    }
+
+    public static IDisposable NavigatePopTo<TParameter, TPage>(
+        this IObservable<TParameter> observable,
+        Page page,
+        Action<TParameter> preNavigation = null,
+        Action<TParameter> postNavigation = null,
+        bool animated = true,
+        bool allowMultiple = false,
+        TimeSpan? multiTapThrottleDuration = null)
+        where TPage : Page
+    {
+        return observable
+            .ThrottleFirst(multiTapThrottleDuration ?? DefaultMultiTapThrottleDuration, RxApp.TaskpoolScheduler)
+            .Where(_ => allowMultiple || !Navigating)
+            .Do(_ => Navigating = true)
+            .ObserveOn(RxApp.MainThreadScheduler)
+            .SelectMany(
+                async parameter =>
+                {
+                    preNavigation?.Invoke(parameter);
+                    var pages = await page.PopTo<TPage>(animated);
+                    postNavigation?.Invoke(parameter);
+
+                    return Unit.Default;
+                })
+            .Do(_ => Navigating = false)
+            .Subscribe();
+    }
+
+    public static IDisposable NavigatePopPage<TParameter>(
+        this IObservable<TParameter> observable,
+        VisualElement element,
+        Action<TParameter> preNavigation = null,
+        Action<TParameter> postNavigation = null,
+        bool animated = true,
+        bool allowMultiple = false,
+        TimeSpan? multiTapThrottleDuration = null)
+    {
+        return observable
+            .ThrottleFirst(multiTapThrottleDuration ?? DefaultMultiTapThrottleDuration, RxApp.TaskpoolScheduler)
+            .Where(_ => allowMultiple || !Navigating)
+            .Do(_ => Navigating = true)
+            .ObserveOn(RxApp.MainThreadScheduler)
+            .SelectMany(
+                async parameter =>
+                {
+                    preNavigation?.Invoke(parameter);
+                    var page = await element.Navigation.PopAsync(animated);
+                    postNavigation?.Invoke(parameter);
+
+                    return Unit.Default;
+                })
+            .Do(_ => Navigating = false)
+            .Subscribe();
+    }
+
+    public static IDisposable NavigatePopToRoot<TParameter>(
+        this IObservable<TParameter> observable,
+        VisualElement element,
+        Action<TParameter> preNavigation = null,
+        Action<TParameter> postNavigation = null,
+        bool animated = true,
+        bool allowMultiple = false,
+        TimeSpan? multiTapThrottleDuration = null)
+    {
+        return observable
+            .ThrottleFirst(multiTapThrottleDuration ?? DefaultMultiTapThrottleDuration, RxApp.TaskpoolScheduler)
+            .Where(_ => allowMultiple || !Navigating)
+            .Do(_ => Navigating = true)
+            .ObserveOn(RxApp.MainThreadScheduler)
+            .SelectMany(
+                async parameter =>
+                {
+                    preNavigation?.Invoke(parameter);
+                    await element.Navigation.PopToRootAsync(animated);
+                    postNavigation?.Invoke(parameter);
+
+                    return Unit.Default;
+                })
+            .Do(_ => Navigating = false)
+            .Subscribe();
+    }
+
+    public static IDisposable NavigateToModalPage<TPage>(
+        this IObservable<Unit> observable,
+        VisualElement element,
+        Action<TPage, Unit> preNavigation = null,
+        Action<TPage, Unit> postNavigation = null,
+        bool animated = true,
+        bool allowMultiple = false,
+        IScheduler pageCreationScheduler = null,
+        TimeSpan? multiTapThrottleDuration = null)
+        where TPage : Page, IStellarView
+    {
+        return NavigateToModalPage<Unit, TPage>(observable, element, preNavigation, postNavigation, animated, allowMultiple, pageCreationScheduler, multiTapThrottleDuration);
+    }
+
+    public static IDisposable NavigateToModalPage<TParameter, TPage>(
+        this IObservable<TParameter> observable,
+        VisualElement element,
+        Action<TPage, TParameter> preNavigation = null,
+        Action<TPage, TParameter> postNavigation = null,
+        bool animated = true,
+        bool allowMultiple = false,
+        IScheduler pageCreationScheduler = null,
+        TimeSpan? multiTapThrottleDuration = null)
+        where TPage : Page, IStellarView
+    {
+        return observable
+            .ThrottleFirst(multiTapThrottleDuration ?? DefaultMultiTapThrottleDuration, RxApp.TaskpoolScheduler)
+            .Where(_ => allowMultiple || !Navigating)
+            .Do(_ => Navigating = true)
+            .ObserveOn(pageCreationScheduler ?? RxApp.TaskpoolScheduler)
+            .Select(
+                x =>
+                {
+                    var page = element.GetPage<TPage>();
+                    return (Page: page, Parameter: x, IsAppearingTask: page.AppearingAsync());
+                })
+            .ObserveOn(RxApp.MainThreadScheduler)
+            .SelectMany(
+                async x =>
+                {
+                    preNavigation?.Invoke(x.Page, x.Parameter);
+                    await Task.WhenAll(
+                        x.IsAppearingTask,
+                        element.Navigation.PushModalAsync(x.Page, animated));
+                    postNavigation?.Invoke(x.Page, x.Parameter);
+
+                    return Unit.Default;
+                })
+            .Do(_ => Navigating = false)
+            .Subscribe();
+    }
+
+    public static IDisposable NavigateToModalPage<TParameter, TPage>(
+        this IObservable<TParameter> observable,
+        VisualElement element,
+        Func<TParameter, TPage> pageCreator,
+        Action<TPage, TParameter> preNavigation = null,
+        Action<TPage, TParameter> postNavigation = null,
+        bool animated = true,
+        bool allowMultiple = false,
+        IScheduler pageCreationScheduler = null,
+        TimeSpan? multiTapThrottleDuration = null)
+        where TPage : Page, IStellarView
+    {
+        return observable
+            .ThrottleFirst(multiTapThrottleDuration ?? DefaultMultiTapThrottleDuration, RxApp.TaskpoolScheduler)
+            .Where(_ => allowMultiple || !Navigating)
+            .Do(_ => Navigating = true)
+            .ObserveOn(pageCreationScheduler ?? RxApp.TaskpoolScheduler)
+            .Select(
+                x =>
+                {
+                    var page = pageCreator.Invoke(x);
+                    return (Page: page, Parameter: x, IsAppearingTask: page.AppearingAsync());
+                })
+            .ObserveOn(RxApp.MainThreadScheduler)
+            .SelectMany(
+                async x =>
+                {
+                    preNavigation?.Invoke(x.Page, x.Parameter);
+                    await Task.WhenAll(
+                        x.IsAppearingTask,
+                        element.Navigation.PushModalAsync(x.Page, animated));
+                    postNavigation?.Invoke(x.Page, x.Parameter);
+
+                    return Unit.Default;
+                })
+            .Do(_ => Navigating = false)
+            .Subscribe();
+    }
+
+    public static IDisposable NavigatePopModalPage<TParameter>(
+        this IObservable<TParameter> observable,
+        VisualElement element,
+        Action<TParameter> preNavigation = null,
+        Action<TParameter> postNavigation = null,
+        bool animated = true,
+        bool allowMultiple = false,
+        TimeSpan? multiTapThrottleDuration = null)
+    {
+        return observable
+            .ThrottleFirst(multiTapThrottleDuration ?? DefaultMultiTapThrottleDuration, RxApp.TaskpoolScheduler)
+            .Where(_ => allowMultiple || !Navigating)
+            .Do(_ => Navigating = true)
+            .ObserveOn(RxApp.MainThreadScheduler)
+            .SelectMany(
+                async parameter =>
+                {
+                    preNavigation?.Invoke(parameter);
+                    var page = await element.Navigation.PopModalAsync(animated);
+                    postNavigation?.Invoke(parameter);
+
+                    Navigating = false;
+
+                    return Unit.Default;
+                })
+            .Subscribe();
+    }
+}
---
+++ EightBot.Stellar.Maui/Platforms/Android/Effects/KeyboardReturnKeyTypeNameEffect.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using Android.Views;
+using Android.Views.InputMethods;
+using Android.Widget;
+using EightBot.Stellar.Maui.AttachedProperties;
+using EightBot.Stellar.Maui.Effects;
+using EightBot.Stellar.Maui.Platforms.Android.Effects;
+using Microsoft.Maui.Controls;
+
+[assembly: ExportEffect(typeof(KeyboardReturnKeyTypeNameEffect), nameof(KeyboardReturnKeyTypeNameEffect))]
+
+namespace EightBot.Stellar.Maui.Platforms.Android.Effects;
+
+public class KeyboardReturnKeyTypeNameEffect : Microsoft.Maui.Controls.Platform.PlatformEffect
+{
+    private ImeAction _startingAction;
+    private string _startingImeActionLabel;
+
+    public static string EffectName => EffectNames.KeyboardReturnKeyTypeNameEffect;
+
+    protected override void OnAttached()
+    {
+        var editText = Control as EditText;
+
+        if (editText == null)
+        {
+            return;
+        }
+
+        _startingAction = editText.ImeOptions;
+        _startingImeActionLabel = editText.ImeActionLabel;
+
+        editText.EditorAction -= EditText_EditorAction;
+        editText.EditorAction += EditText_EditorAction;
+
+        SetReturnType();
+    }
+
+    protected override void OnDetached()
+    {
+        var editText = Control as EditText;
+
+        if (editText == null || editText.Handle == IntPtr.Zero)
+        {
+            return;
+        }
+
+        editText.EditorAction -= EditText_EditorAction;
+
+        editText.ImeOptions = _startingAction;
+        editText.SetImeActionLabel(_startingImeActionLabel, _startingAction);
+    }
+
+    protected override void OnElementPropertyChanged(System.ComponentModel.PropertyChangedEventArgs args)
+    {
+        if (args.PropertyName.Equals(KeyboardReturnKeyTypeProperty.KeyboardReturnKeyTypeName))
+        {
+            SetReturnType();
+            return;
+        }
+
+        base.OnElementPropertyChanged(args);
+    }
+
+    private void SetReturnType()
+    {
+        var editText = Control as EditText;
+
+        if (editText == null)
+        {
+            return;
+        }
+
+        var keyboardType = KeyboardReturnKeyTypeProperty.GetKeyboardReturnKeyType(Element);
+
+        switch (keyboardType)
+        {
+            case EntryKeyboardReturnType.Go:
+                editText.ImeOptions = ImeAction.Go;
+                editText.SetImeActionLabel("Go", ImeAction.Go);
+                break;
+            case EntryKeyboardReturnType.Next:
+                editText.ImeOptions = ImeAction.Next;
+                editText.SetImeActionLabel("Next", ImeAction.Next);
+                break;
+            case EntryKeyboardReturnType.Send:
+                editText.ImeOptions = ImeAction.Send;
+                editText.SetImeActionLabel("Send", ImeAction.Send);
+                break;
+            case EntryKeyboardReturnType.Search:
+                editText.ImeOptions = ImeAction.Search;
+                editText.SetImeActionLabel("Search", ImeAction.Search);
+                break;
+            case EntryKeyboardReturnType.Default:
+                editText.ImeOptions = _startingAction;
+                editText.SetImeActionLabel(_startingImeActionLabel, _startingAction);
+                break;
+        }
+    }
+
+    private void EditText_EditorAction(object sender, TextView.EditorActionEventArgs e)
+    {
+        if (e.ActionId.Equals(ImeAction.Next))
+        {
+            var nextElement = KeyboardReturnKeyTypeProperty.GetNextVisualElement(Element);
+
+            if (nextElement != null)
+            {
+                nextElement.Focus();
+            }
+        }
+    }
+}
