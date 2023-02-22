8d1935ee059fafa306b9f9e3af1a199cb932293f
Michael Stonis <michael.stonis@eightbot.com>
2023-02-21 19:47:52 -0600

    string extensions

---
+++ Stellar/Extensions/StringExtensions.cs
@@ -0,0 +1,29 @@
+﻿namespace Stellar;
+
+public static class StringExtensions
+{
+    public static bool Contains(this string s, string innerString, StringComparison comparisonType)
+    {
+        return s.IndexOf(innerString, comparisonType) >= 0;
+    }
+
+    public static bool IsNullOrEmpty(this string s)
+    {
+        return string.IsNullOrEmpty(s);
+    }
+
+    public static bool IsNotNullOrEmpty(this string s)
+    {
+        return !string.IsNullOrEmpty(s);
+    }
+
+    public static bool IsNullOrWhiteSpace(this string s)
+    {
+        return string.IsNullOrWhiteSpace(s);
+    }
+
+    public static bool IsNotNullOrWhiteSpace(this string s)
+    {
+        return !string.IsNullOrWhiteSpace(s);
+    }
+}
