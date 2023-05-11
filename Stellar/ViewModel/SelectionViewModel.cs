975333e7d0a2aeca2f797863d4d5d31abb47d47a
Michael Stonis <michael.stonis@eightbot.com>
2023-05-11 16:53:32 -0500

    feat: Add SelectionViewModel class
    This commit adds a new file, SelectionViewModel.cs, which contains the implementation of the SelectionViewModel<TSelectedValKey> class. This class inherits from ViewModelBase and includes properties for Key, DisplayValue, Selected, and ToggleSelected. The RegisterObservables method is also overridden to register the ToggleSelected command.

---
+++ Stellar/ViewModel/SelectionViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using ReactiveUI.Fody.Helpers;
+
+namespace Stellar.ViewModel;
+
+public class SelectionViewModel<TSelectedValKey> : ViewModelBase
+{
+    [Reactive]
+    public TSelectedValKey Key { get; set; }
+
+    [Reactive]
+    public string DisplayValue { get; set; }
+
+    [Reactive]
+    public bool Selected { get; set; }
+
+    [Reactive]
+    public ReactiveCommand<Unit, bool> ToggleSelected { get; private set; }
+
+    protected override void RegisterObservables()
+    {
+        ToggleSelected =
+            ReactiveCommand
+                .Create(() => Selected = !Selected)
+                .DisposeWith(ViewModelBindings);
+    }
+}
