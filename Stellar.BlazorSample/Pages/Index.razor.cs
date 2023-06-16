5ef793611e6107e35cac2357a9db84cdb737ce45
Michael Stonis <michael.stonis@eightbot.com>
2023-06-16 13:07:42 -0500

    Update Stellar.BlazorSample project dependencies
    - Update Microsoft.AspNetCore.Components.WebAssembly to version 7.0.7
    - Update Microsoft.AspNetCore.Components.WebAssembly.DevServer to version 7.0.7
    
    Initialize StellarComponent in Index page
    
    - Add System.Reactive.Linq and ReactiveUI using statements
    - Call InitializeStellarComponent method in OnInitialized override of Index page

--- Stellar.BlazorSample/Pages/Index.razor.cs
+++ Stellar.BlazorSample/Pages/Index.razor.cs
@@ -1,10 +1,18 @@
-﻿using Stellar.Blazor;
+﻿using System.Reactive.Linq;
+using ReactiveUI;
+using Stellar.Blazor;
 using Stellar.BlazorSample.ViewModels;
 
 namespace Stellar.BlazorSample.Pages;
 
 public partial class Index : InjectableComponentBase<IndexViewModel>
 {
+    protected override void OnInitialized()
+    {
+        base.OnInitialized();
+        this.InitializeStellarComponent();
+    }
+
     public override void BindControls()
     {
     }
