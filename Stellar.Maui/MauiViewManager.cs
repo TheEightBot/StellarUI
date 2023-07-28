60d448fa6dd75ff9ba8028c4f3136cdea1c19cd4
Michael Stonis <120685+michaelstonis@users.noreply.github.com>
2023-07-28 16:01:54 -0500

    Add HotReloadService and HandleHotReload method for debugging
    - Added HotReloadService.cs file with HotReloadService class
    - Added event Action<Type[]?> UpdateApplicationEvent in HotReloadService class
    - Added ClearCache and UpdateApplication methods in HotReloadService class
    - In MauiViewManager class, added _view field for debugging purposes
    - In HandlerChanging method of MauiViewManager, subscribed to the UpdateApplicationEvent and called HandleHotReload method when a new handler is set
    - Added HandleHotReload method in MauiViewManager to handle hot reload functionality

--- Stellar.Maui/MauiViewManager.cs
+++ Stellar.Maui/MauiViewManager.cs
@@ -1,19 +1,45 @@
 ﻿using ReactiveUI;
+using Stellar.ViewModel;
 
 namespace Stellar.Maui;
 
 public class MauiViewManager : ViewManager
 {
+#if DEBUG
+    private IStellarView? _view;
+#endif
+
     public void HandlerChanging<TViewModel>(IStellarView<TViewModel> view, HandlerChangingEventArgs args)
         where TViewModel : class
     {
         if (args.NewHandler is not null)
         {
+#if DEBUG
+            _view = view;
+            HotReloadService.UpdateApplicationEvent -= HandleHotReload;
+            HotReloadService.UpdateApplicationEvent += HandleHotReload;
+#endif
+
             HandleActivated(view);
 
             return;
         }
 
         HandleDeactivated(view);
+#if DEBUG
+        HotReloadService.UpdateApplicationEvent -= HandleHotReload;
+#endif
     }
+
+#if DEBUG
+    private void HandleHotReload(Type[] updatedTypes)
+    {
+        if (_view is null)
+        {
+            return;
+        }
+
+        _view.SetupUserInterface();
+    }
+#endif
 }
