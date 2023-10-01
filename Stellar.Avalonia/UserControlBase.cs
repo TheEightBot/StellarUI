9d64ae63449868e1ab5b2a8424ae9a00ab5296d2
Michael Stonis <120685+michaelstonis@users.noreply.github.com>
2023-10-01 10:15:39 -0500

    Refactor UserControlBase.cs for Stellar.Avalonia
    - Remove unused using statements
    - Add a new abstract class `UserControlBase<TViewModel, TDataModel>` that extends `ReactiveUserControl<TViewModel>` and implements `IStellarView<TViewModel>`
    - Implement properties and methods required by `IStellarView<TViewModel>`
    - Override `OnAttachedToVisualTree` and `OnDetachedFromVisualTree` to handle view activation and deactivation
    - Override `OnPropertyChanged` to handle property changes
    - Override `OnDataContextEndUpdate` to map the data model to the view model if applicable
    - Implement disposal logic in the Dispose method

--- Stellar.Avalonia/UserControlBase.cs
+++ Stellar.Avalonia/UserControlBase.cs
@@ -1,9 +1,7 @@
-﻿using System;
-using System.ComponentModel;
+﻿using System.ComponentModel;
 using System.Reactive;
 using System.Reactive.Disposables;
 using Avalonia;
-using Avalonia.Controls;
 using Avalonia.ReactiveUI;
 
 namespace Stellar.Avalonia;
@@ -55,6 +53,78 @@ public abstract class UserControlBase<TViewModel> : ReactiveUserControl<TViewMod
         ViewManager.PropertyChanged<UserControlBase<TViewModel>, TViewModel>(this, change.Property.Name);
 
         base.OnPropertyChanged(change);
+    }
+
+    protected virtual void Dispose(bool disposing) =>
+        this.ManageDispose(disposing, ref _isDisposed);
+
+    public void Dispose()
+    {
+        // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+        Dispose(disposing: true);
+        GC.SuppressFinalize(this);
+    }
+}
+
+public abstract class UserControlBase<TViewModel, TDataModel> : ReactiveUserControl<TViewModel>, IStellarView<TViewModel>
+    where TViewModel : class
+{
+    private bool _isDisposed;
+
+    [EditorBrowsable(EditorBrowsableState.Never)]
+    public ViewManager ViewManager { get; } = new AvaloniaViewManager<TViewModel>();
+
+    public IObservable<Unit> UserControlInitialized => ViewManager.Initialized;
+
+    public IObservable<Unit> Activated => ViewManager.Activated;
+
+    public IObservable<Unit> Deactivated => ViewManager.Deactivated;
+
+    public IObservable<Unit> Disposed => ViewManager.Disposed;
+
+    public IObservable<LifecycleEvent> Lifecycle => ViewManager.Lifecycle;
+
+    public virtual void Initialize()
+    {
+    }
+
+    public abstract void SetupUserInterface();
+
+    public abstract void Bind(CompositeDisposable disposables);
+
+    protected abstract void MapDataModelToViewModel(TViewModel viewModel, TDataModel dataModel);
+
+    protected override void OnAttachedToVisualTree(VisualTreeAttachmentEventArgs e)
+    {
+        base.OnAttachedToVisualTree(e);
+
+        ViewManager.HandleActivated(this);
+    }
+
+    protected override void OnDetachedFromVisualTree(VisualTreeAttachmentEventArgs e)
+    {
+        ViewManager.OnLifecycle(LifecycleEvent.IsDisappearing);
+
+        ViewManager.HandleDeactivated(this);
+
+        base.OnDetachedFromVisualTree(e);
+    }
+
+    protected override void OnPropertyChanged(AvaloniaPropertyChangedEventArgs change)
+    {
+        ViewManager.PropertyChanged<UserControlBase<TViewModel, TDataModel>, TViewModel>(this, change.Property.Name);
+
+        base.OnPropertyChanged(change);
+    }
+
+    protected override void OnDataContextEndUpdate()
+    {
+        base.OnDataContextEndUpdate();
+
+        if (ViewModel is not null && DataContext is TDataModel dataModel)
+        {
+            MapDataModelToViewModel(ViewModel, dataModel);
+        }
     }
 
     protected virtual void Dispose(bool disposing) =>
